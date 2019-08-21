4d350cedb47740a193d2967ff5aae59886c68f02
vikasp <vikasp@bsf.io>
2019-08-21 17:50:25 +0530

    Version updated to 1.2.0

--- astra-bulk-edit.php
+++ astra-bulk-edit.php
@@ -1,22 +1,21 @@
 <?php
 /**
  * Plugin Name: Astra Bulk Edit
- * Plugin URI: http://www.wpastra.com/pro/
+ * Plugin URI:  http://www.wpastra.com/pro/
  * Description: Easier way to edit Astra meta options in bulk.
- * Version: 1.1.0
+ * Version: 1.2.0
  * Author: Brainstorm Force
- * Author URI: http://www.brainstormforce.com
+ * Author URI: https://www.brainstormforce.com
  * Domain Path: /languages
  * Text Domain: astra-bulk-edit
- * Version:         1.0.0
  *
- * @package         Astra_Bulk_Edit
+ * @package Astra Bulk Edit
  */
 
 /**
  * Set constants.
  */
-define( 'ASTRA_BLK_VER',  '1.1.0' );
+define( 'ASTRA_BLK_VER', '1.2.0' );
 define( 'ASTRA_BLK_FILE', __FILE__ );
 define( 'ASTRA_BLK_BASE', plugin_basename( ASTRA_BLK_FILE ) );
 define( 'ASTRA_BLK_DIR',  plugin_dir_path( ASTRA_BLK_FILE ) );
